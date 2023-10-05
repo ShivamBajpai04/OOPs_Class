1f5b239a0a0cd19a0ad6e66b13e6d6681720be55
Shivam Bajpai <118303585+ShivamBajpai04@users.noreply.github.com>
2023-10-05 18:21:30 +0530

    Bank and Opps

--- Oops_Intro.cpp
+++ Oops_Intro.cpp
@@ -1,29 +1,84 @@
 #include <iostream>
 using namespace std;
-//Given a list of students with name,roll number and marks, sort them in descending order of marks.
+//Given a list of students with name,roll number and subject marks,find average of marks.
+
+class Subject{
+    public:
+        int marks;
+};
+
 class Student{
     public:
         string name;
         int Rno;
-        int marks;
+        Subject oops,CN,DBMS;
+        void print(){
+            cout<<name<<" "<<Rno<<" "<<oops.marks<<" "<<CN.marks<<" "<<DBMS.marks<<endl;
+        }
+
+        int average(int a,int b,int c){
+            return (a+b+c)/3;
+        }
+};
+
+//employees
+
+class Employees{
+    public:
+        string name;
+    private:
+        unsigned int salary;
+    public:
+
+        void setsal(int amount){
+            salary=amount;
+        }
+
+        void raise(){
+            if(salary<20000){
+                salary+=(salary)/5;
+            }
+            else if(salary<30000){
+                salary+=(salary/100)*15;
+            }
+            else if(salary>=30000){
+                salary+=(salary/100)*30;
+            }
+        }
+
+        int getsal(){
+            return salary;
+        }
+
+        void display(){
+            cout<<getsal()<<" "<<name;
+        }
 };
 
 int main(){
+    //q1 driver
+    
     int n;
     cin>>n;
     Student arr[n];
+
+    //input
     for(int i=0;i<n;i++){
-        cin>>arr[i].name>>arr[i].Rno>>arr[i].marks;
-    }
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i].marks<arr[j].marks){
-                swap(arr[i],arr[j]);
-            }
-        }
+        cin>>arr[i].name>>arr[i].Rno>>arr[i].oops.marks>>arr[i].CN.marks>>arr[i].DBMS.marks;
     }
 
+    //average
     for(int i=0;i<n;i++){
-        cout<<arr[i].name<<" "<<arr[i].marks<<endl;
+        cout<<arr[i].average(arr[i].oops.marks,arr[i].CN.marks,arr[i].DBMS.marks)<<endl;
     }
+
+    //q2 driver    
+
+    Employees e1;
+    int amt;
+    cin>>e1.name>>amt;
+    e1.setsal(amt);
+    e1.raise();
+    e1.display();
 }
+    
---
+++ bank.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+class Account{
+    private:
+        int pwd;
+        unsigned int balance=0;
+    public:
+        string Acc_holder_name;
+        int Acc_no;
+
+
+        void set_pwd(int p){
+            pwd = p;
+        }
+
+        bool check_pwd(int p){
+            if(p == pwd){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        void deposit(int amount){
+            balance += amount;
+        }
+
+        void withdraw(int amount){
+            if(amount <= balance){
+                balance -= amount;
+            }
+        }
+
+        void transfer(Account &other, int amount){
+            if(amount <= balance){
+                balance -= amount;
+                other.balance += amount;
+            }
+            else{
+                cout<<"insufficient balance";
+            }
+        }
+
+        void display_balance(){
+            cout<<balance<<endl;
+        }
+};
+
+int main(){
+    Account a1,a2;
+    int pw;
+    cin>>a1.Acc_holder_name>>a1.Acc_no>>pw;
+    cin>>a2.Acc_holder_name>>a2.Acc_no;
+    a1.set_pwd(pw);
+
+    bool authorised= false;
+    int pwd;
+    cout<<"enter pwd ";
+    cin>>pwd;
+    if(a1.check_pwd(pwd)){ 
+        
+        authorised = true;
+        cout<<"authorised";
+    }
+    else{
+        cout<<"unauthorised";
+    }
+    while(authorised){
+        int choice;
+        cout<<"press 1 to check balance | 2 - deposit | 3 - widraw | 4 - transfer";
+        cin>>choice;
+        switch (choice){
+            case 1:
+                a1.display_balance();
+                break;
+            case 2:
+                int amount;
+                cin>>amount;
+                a1.deposit(amount);
+                break;
+            case 3:
+                cin>>amount;
+                a1.withdraw(amount);
+                break;
+            case 4:
+                int amt;
+                cin>>amt;
+                a1.transfer(a2,amt);
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+}
